f0c7c0e135af3d7cdfb76e2bbd89710630b6e85c
Kristján Oddsson <koddsson@gmail.com>
2015-05-13 09:34:47 +0000

    Remove premature return blunder.

--- service-worker.js
+++ service-worker.js
@@ -25,11 +25,6 @@ self.addEventListener('fetch', function(event) {
         var fetchRequest = event.request.clone();
 
         return fetch(fetchRequest).then(function(response) {
-          // Check if we received a valid response
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
           // IMPORTANT: Clone the response. A response is a stream
           // and because we want the browser to consume the response
           // as well as the cache consuming the response, we need
@@ -37,7 +32,7 @@ self.addEventListener('fetch', function(event) {
           var responseToCache = response.clone();
 
           caches.open(CACHE_NAME)
-          .then(function(cache) {
+            .then(function(cache) {
             cache.put(event.request, responseToCache);
           });
 
