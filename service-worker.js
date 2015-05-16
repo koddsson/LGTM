9b8e8501f2f9eca117784fc10eaa1f05d5eaf3d2
Kristján Oddsson <koddsson@gmail.com>
2015-05-16 23:29:37 +0000

    [v2] Re-write service worker to use thumbnails
    The thumbnails are served up when the page loads initially and the
    service worker is fetching the full images in the background, sticking
    them into cache. Once the full image is loaded it will serve that up
    instead of the thumbnail.

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,50 @@
-const CACHE_NAME = 'LGTM';
+'use strict';
+
+const CACHE_NAME = 'LGTM-v2';
 
 var isImageRequest = function(request) {
-  return (request.method === 'GET' &&
-          request.headers.get('accept').indexOf('image') !== -1);
+  return request.method === 'GET' && request.context === 'image';
 };
 
+self.addEventListener('install', function(event) {
+  console.log('Installing');
+  console.log('Adding large images to cache!');
+  fetch('data.json').then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    caches.open(CACHE_NAME).then(function(cache) {
+      json.forEach(function(item) {
+        console.log(`Adding ${item.url} to cache`);
+        fetch(item.url).then(function(response) {
+          let largeUrl = item.url
+            .replace('.gif', 's.gif')
+            .replace('.png', 's.png')
+            .replace('.jpg', 's.jpg');
+          cache.put(largeUrl, response);
+        });
+      });
+    });
+  });
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activating');
+});
+
 self.addEventListener('fetch', function(event) {
+  console.log('fetching');
   if (!isImageRequest(event.request)) {
-    event.respondWith(fetch(event.request));
+    return;
   } else {
+    let cleanURL = event.request.url.substring(0, event.request.url.indexOf('?'));
     event.respondWith(
-      caches.match(event.request)
-      .then(function(response) {
+      caches.match(cleanURL).then(function(response) {
         if (response) {
           console.log('Cache hit!');
-          return response;
+        } else {
+          console.log('Cache miss!');
         }
-
-        console.log('Cache miss!');
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(function(response) {
-          // IMPORTANT: Clone the response. A response is a stream
-          // and because we want the browser to consume the response
-          // as well as the cache consuming the response, we need
-          // to clone it so we have 2 stream.
-          var responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-
-          return response;
-        });
+        return response || fetch(event.request);
       })
     );
   }
